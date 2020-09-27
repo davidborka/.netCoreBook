3ec4372941f08d45e1fb95c842c9f2d30793b35a
David Borka <david0borka@gmail.com>
2020-09-27 21:40:21 +0200

    Practice Midleware End of Chapter 12

---
+++ Platform/LocationMidleware.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Platform
+{
+	public class LocationMidleware
+	{
+		private RequestDelegate next;
+		private MessageOptions options;
+
+		public LocationMidleware(RequestDelegate nextDelegate, IOptions<MessageOptions> opts)
+		{
+			next = nextDelegate;
+			options = opts.Value;
+		}
+
+		public async Task Invoke(HttpContext context)
+		{
+			if(context.Request.Path == "/location")
+			{
+				await context.Response.WriteAsync($"{options.CityName}, {options.Country}");
+			}
+			else
+			{
+				await next(context);
+			}
+		}
+	}
+}
---
+++ Platform/Midleware.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+
+namespace Platform
+{
+	public class QueryStringMiddleware
+	{
+		private RequestDelegate next;
+
+		public QueryStringMiddleware()
+		{
+
+		}
+		public QueryStringMiddleware(RequestDelegate requestDelegate)
+		{
+			next = requestDelegate;
+		}
+
+		public async Task Invoke(HttpContext context)
+		{
+			if(context.Request.Method == HttpMethods.Get &&
+				context.Request.Query["custom"] == "true")
+			{
+				await context.Response.WriteAsync("Class-based Middleware \n");
+			}
+			if(next != null)
+			{
+				await next(context);
+			}
+		}
+	}
+}
